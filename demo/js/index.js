68cc8e964da1abf630401af64e6de091a1b03e49
Mulgure <Mulgure@users.noreply.github.com>
2016-10-11 19:01:54 +0800

    Add files via upload

--- demo/js/index.js
+++ demo/js/index.js
@@ -6,57 +6,33 @@ $(document).ready(function(){
 		}
 	);
 
-$(".Btn1").click(function(){
-	location='#/index1'
-})
+	$(".Btn1").click(function(){
+		location='#/index1';
+	})
 
-$(".Btn2").click(function(){
-	location='#/index2'
-})
+	$(".Btn2").click(function(){
+		location='#/index2';
+	})
 
-$(".Btn4").click(function(){
-	location='#/index4'
-})
+	$(".Btn4").click(function(){
+		location='#/index4';
+	})
 
-$(".Btn5").click(function(){
-	location='#/index5'
-})
+	$(".Btn5").click(function(){
+		location='#/index5';
+	})
 
-$(".Btn6").click(function(){
-	location='#/index6'
-})
+	$(".Btn6").click(function(){
+		location='#/index6';
+	});
 
-//      表格内全选框
-$(".all").click(function(){    
-    if(this.checked){    
-        $("table .checkbox").attr("checked", true);   
-    }else{    
-        $("table .checkbox").attr("checked", false); 
-    }    
-});  
-/*
-//     点击按钮切换界面
-$(".Btn1").click(function(){ 
-	$(".main").css("display","none")
-	$(".main1").css("display","block")
-})
-$(".Btn2").click(function(){ 
-	$(".main").css("display","none")
-	$(".main2").css("display","block")
-})
+	//      表格内全选框
+	$(".all").click(function(){    
+	   if(this.checked){    
+	        $("table .checkbox").prop("checked", true) 
+	    }else{    
+	        $("table .checkbox").prop("checked", false)
+	    }    
+	});  
 
-$(".Btn4").click(function(){ 
-	$(".main").css("display","none")
-	$(".main4").css("display","block")
-})
-$(".Btn5").click(function(){ 
-	$(".main").css("display","none")
-	$(".main5").css("display","block")
-})
-$(".Btn6").click(function(){ 
-	$(".main").css("display","none")
-	$(".main6").css("display","block")
-})*/
-
-
-});
+}); 
